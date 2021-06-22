4a68548fe008ee8a26ed4bd03b5771a5acb1b1ea
prakash <prakashsellathurai@gmail.com>
2021-06-22 22:10:07 +0530

    ✅ added Quick Sort

---
+++ cpp/Sorting/Quick-sort.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+
+using namespace std;
+
+int partition(int arr[], int l, int h) {
+    int firsthigh,p,i;
+    p = h;
+    firsthigh = l;
+    
+    for(i = l; i < h; ++i) {
+       if(arr[i]<arr[p]){
+           swap(arr[i], arr[firsthigh]);
+           firsthigh++;
+       }
+    }
+    swap(arr[p], arr[firsthigh]);
+    return firsthigh;
+    
+}
+void QuickSort(int arr[], int l, int r) {
+  int p;
+  if ((r - l) > 0) {
+    p = partition(arr, l, r);
+    QuickSort(arr, l, p-1);
+    QuickSort(arr, p + 1, r);
+
+  } else {
+    return;
+  }
+}
+void print_arr(int array[], int n) {
+
+  for (int i = 0; i < n; ++i) {
+    cout << array[i] << " ";
+  }
+
+  std::cout << std::endl;
+}
+int main(int argc, char const *argv[]) {
+
+  int array[] = {22, 4, 5, 4, 8, 7, 8, 7, 8, 9, 10, 1, 0};
+  int n = sizeof(array) / sizeof(array[0]);
+  print_arr(array, n);
+  QuickSort(array, 0, n -1);
+  print_arr(array, n);
+}
