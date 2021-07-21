70db45c985028e257718966ab9b9ae9a0da51d0a
prakash <prakashsellathurai@gmail.com>
2021-07-21 19:00:56 +0530

    update floyd-all-pair-shortest-path.cpp

--- cpp/Graphs/shortest-path/floyd-all-pair-shortest-path.cpp
+++ cpp/Graphs/shortest-path/floyd-all-pair-shortest-path.cpp
@@ -10,6 +10,7 @@
  */
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
 /**
@@ -18,17 +19,29 @@ using namespace std;
  */
 class Graph {
 public:
-  int V;               // No. of vertices
-  int **weight;        // weight of the edge
-  int **shortest_path; // shortest path matrix
-  Graph(int **weight) {
-    this->V = (int)(sizeof(weight) / sizeof(weight[0]));
+  int V;                             // No. of vertices
+  vector<vector<int>> weight;        // weight of the edge
+  vector<vector<int>> shortest_path; // shortest path matrix
+  vector<vector<int>> parent;        // parent matrix
+
+  Graph(vector<vector<int>> weight) {
+    this->V = weight.size();
     this->weight = weight;
     this->shortest_path = weight;
+    this->parent = weight;
+    for (int i = 0; i < this->V; i++) {
+      for (int j = 0; j < this->V; j++) {
+        if (this->weight[i][j]) {
+          this->parent[i][j] = j;
+        } else {
+          this->parent[i][j] = -1;
+        }
+      }
+    }
   }
 
   // floyd algo
-  int **floyd() {
+  vector<vector<int>> floyd() {
 
     int i, j, k;
     int through_k;
@@ -39,36 +52,82 @@ public:
           through_k = this->shortest_path[i][k] + this->shortest_path[k][j];
           if (through_k < this->shortest_path[i][j]) {
             this->shortest_path[i][j] = through_k;
+            this->parent[i][j] = this->parent[i][k];
           }
         }
       }
     }
     return shortest_path;
   }
+  void print_parent() {
+    std::cout << "    ";
+    for (int i = 0; i < this->V; i++) {
+      std::cout << i << " ";
+    }
+    cout << endl;
+    std::cout << "    ";
+    for (int i = 0; i < this->V; i++) {
+      std::cout << "- ";
+    }
+    cout << endl;
+    for (int i = 0; i < this->V; i++) {
+      std::cout << i << " | ";
+      for (int j = 0; j < this->V; j++) {
+        cout << this->parent[i][j] << " ";
+      }
+      cout << endl;
+    }
+  }
+  // print path from a source to a destination
+  void print_path(int source, int destination) {
+      vector<int> path = {source};
+      while (source != destination) {
+          source= this->parent[source][destination];
+          path.push_back(source);
+      }
+      for (int i = 0; i < path.size(); i++) {
+          std::cout << path[i] << " ";
+      }
+      std::cout << endl;
+  }
+  // print the shortest path matrix
+  void print_shortest_path() {
+    std::cout << "    ";
+    for (int i = 0; i < this->V; i++) {
+      std::cout << i << " ";
+    }
+    cout << endl;
+    std::cout << "    ";
+    for (int i = 0; i < this->V; i++) {
+      std::cout << "- ";
+    }
+    cout << endl;
+    for (int i = 0; i < this->V; i++) {
+      std::cout << i << " | ";
+      for (int j = 0; j < this->V; j++) {
+        cout << this->shortest_path[i][j] << " ";
+      }
+      cout << endl;
+    }
+  }
+
   ~Graph() {
-    delete[] weight;
+    weight.clear();
     this->V = 0;
   }
 };
 
 int main(int argc, const char **argv) {
-  int **weight = new int *[5];
-  for (int i = 0; i < 5; i++) {
-    weight[i] = new int[5];
-  }
-  weight[1][2] = 5;
-  weight[1][3] = 8;
-  weight[1][4] = -4;
-  weight[2][3] = 7;
-  weight[2][4] = -2;
-  weight[3][4] = 3;
+  vector<vector<int>> weight = {{5, 8, 2, 5, 50},
+                                {8, 1, 2, 3, 7},
+                                {2, 1, 3, 4, 5},
+                                {5, 3, 4, 1, 2},
+                                {50, 7, 5, 2, 1}};
+
   Graph g(weight);
-  int **shortest_path = g.floyd();
-  for (int i = 1; i <= 4; i++) {
-    for (int j = 1; j <= 4; j++) {
-      cout << "Shortest path between " << i << " and " << j << " is "
-           << shortest_path[i][j] << endl;
-    }
-  }
+  g.floyd();
+  g.print_shortest_path();
+ g.print_path(0, 4);
+ 
   return 0;
 }
