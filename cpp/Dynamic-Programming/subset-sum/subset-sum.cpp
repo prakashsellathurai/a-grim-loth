d21fa4cd583fbef761c54c37e18d4c8910a9a790
prakash <prakashsellathurai@gmail.com>
2021-08-20 20:23:32 +0530

    ✅ update subset-sum.cpp

--- cpp/Dynamic-Programming/subset-sum/subset-sum.cpp
+++ cpp/Dynamic-Programming/subset-sum/subset-sum.cpp
@@ -1,29 +1,57 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main(int argc, char const *argv[]) {
-  int A[] = {1, 2, 3, 4};
-  int n = sizeof(A) / sizeof(A[0]);
-  int M = 6;
+#include <algorithm>
+#include <iostream>
+#include <vector>
 
-  // Solution
-  int m[M + 10];
-  for (int i = 0; i < M+ 10; i++)
-    m[i] = 0;
-  m[0] = 1;
-  for (int i = 0; i < n; i++) {
-    for (int j = M; j >= A[i]; j--) {
-      m[j] |= m[j - A[i]];
+using namespace std;
+#define MAXSIZE 20
+bool dp[MAXSIZE][MAXSIZE];
+int parent[MAXSIZE][MAXSIZE];
+
+void report_subset(int n, int k) {
+  if (k == 0) {
+    return;
+  }
+  if (parent[n][k] == -1) {
+    report_subset(n - 1, k);
+  } else {
+
+    report_subset(n - 1, parent[n][k]);
+    cout << k - parent[n][k] << " ";
+  }
+}
+int subset_sum(int arr[], int n, int k) {
+
+  dp[0][0] = true;
+  parent[0][0] = -1;
+
+  for (int i = 1; i <= k; i++) {
+    dp[0][i] = false;
+    parent[0][i] = -1;
+  }
+
+  for (int i = 1; i <= n; i++) {
+    for (int j = 0; j <= k; j++) { // j is the number of elements in the subset
+      dp[i][j] = dp[i - 1][j];
+      parent[i][j] = -1;
+
+      if ((j >= arr[i - 1]) && (dp[i - 1][j - arr[i - 1]] == true)) {
+        dp[i][j] = true;
+        parent[i][j] = j - arr[i - 1];
+      }
     }
   }
-  for (int i = 0; i < M+ 10; i++) {
-    std::cout << m[i] << '\t';
+  cout << "is subset sum possible " << (dp[n][k] ? "yes" : "no") << endl;
+  if (dp[n][k]) {
+    cout << "The subset is : ";
+    report_subset(n, k);
   }
-  std::cout  << std::endl;
- 
-    for (int i = 0; i < M+ 10; i++) {
-    std::cout << i << '\t';
-  }
-   std::cout  << std::endl;
-   std::cout << "is subset froms the sum of "<< M <<"? "<< (m[M] ? " True":" False") << std::endl;
+  return dp[n][k];
+}
+
+int main(int argc, char const *argv[]) {
+  int arr[] = {1, 2, 4, 8};
+  int n = sizeof(arr) / sizeof(arr[0]);
+  int k = 11;
+  subset_sum(arr, n, k);
   return 0;
 }
