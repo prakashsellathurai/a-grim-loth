1feb9b9e4faf55402e59cfba8a0e1472bedbc5ae
prakash <prakashsellathurai@gmail.com>
2021-11-09 17:42:33 +0530

    ✨ feat: create 0-1-matrix.py

---
+++ python/dynamic-programming/0-1-matrix.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+ https://leetcode.com/problems/01-matrix
+"""
+__author__ = "prakashsellathurai"
+__copyright__ = "Copyright 2021"
+__version__ = "1.0.1"
+__email__ = "prakashsellathurai@gmail.com"
+
+
+
+from collections import deque
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        m = len(mat)
+        n = len(mat[0])
+        
+        dist = [[ sys.maxsize for j in range(n)] for i in range(m)]
+        
+        
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j]:
+                    if i > 0:
+                        dist[i][j] = min(dist[i][j],dist[i-1][j]+1)
+                    if j > 0:
+                        dist[i][j] = min(dist[i][j],dist[i][j-1]+1)
+                else:
+                    dist[i][j] = 0
+                    
+                    
+        for i in range(m-1,-1,-1):
+            for j in range(n-1,-1,-1):
+                if i < m-1:
+                    dist[i][j] = min(dist[i][j],dist[i+1][j]+1)
+                if j < n-1:
+                    dist[i][j] = min(dist[i][j],dist[i][j+1]+1)
+
+        return dist
+        
