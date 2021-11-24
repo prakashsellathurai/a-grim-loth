d6c631378ceb564db5321a18514aa43ecfc04c6e
Prakash Sellathurai <prakashsellathurai@gmail.com>
2021-11-24 13:53:51 +0000

    findKthLargest sorting added

---
+++ python/problems/Find-Klargest-number-in-unsorted-array/sorting-nlogn.py
@@ -0,0 +1,21 @@
+from typing import List
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+      """
+      >>> Solution().findKthLargest([3,2,1,5,6,4],2)
+      5
+      >>> Solution().findKthLargest([3,2,3,1,2,4,5,5,6],4)
+      4
+      """
+      nums.sort()
+      nums = nums[::-1]
+      return nums[k-1]
+
+import doctest
+doctest.testmod(verbose=2)
+
+
+
+
+
+        
